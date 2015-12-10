8531d0c57cd8efe3dbf63477c66de3088c494ccf
Daniel Monteagudo <dmonte3210@gmail.com>
2015-12-10 18:50:08 -0500

    Driver and all subclasses work

--- Driver.java
+++ Driver.java
@@ -16,24 +16,24 @@ public class Driver{
 	  }
       }
 
-      /* //UNCOMMENT THIS WHEN YOU DO Volume
+      //UNCOMMENT THIS WHEN YOU DO Volume
       public static Volume makeRandomVolumeShape(){
-      String name = ""+letter()+"-3d";
-      double value = Math.round(Math.random()*10.0);
-      double choice = Math.random();
-      if(choice < 0.33){
-      return new Sphere(value,name);
-      }else{
-      double value2 = Math.round(Math.random()*10.0);
-      if(choice < 0.66){
-      return new Cylinder(value,value2,name);
-      }else{
-      double value3 = Math.round(Math.random()*10.0);
-      return new Prism(value,value2,value3,name);
+	  String name = ""+letter()+"-3d";
+	  double value = Math.round(Math.random()*10.0);
+	  double choice = Math.random();
+	  if(choice < 0.33){
+	      return new Sphere(value,name);
+	  }else{
+	      double value2 = Math.round(Math.random()*10.0);
+	      if(choice < 0.66){
+		  return new Cylinder(value,value2,name);
+	      }else{
+		  double value3 = Math.round(Math.random()*10.0);
+		  return new Prism(value,value2,value3,name);
+	      }
+	  }
       }
-      }
-      }
-    */
+ 
 
     public static void main(String[]args){
 	
@@ -49,17 +49,16 @@ public class Driver{
 	  }
 	
 
-	/*//UNCOMMENT THIS WHEN YOU DO VOLUME
+	//UNCOMMENT THIS WHEN YOU DO VOLUME
 	  Volume[] threes = new Volume[6];
 	  for(int i = 0; i < threes.length; i++){
-	  threes[i]=makeRandomVolumeShape();
+	      threes[i]=makeRandomVolumeShape();
 	  }
 	  System.out.println("\nVolume tests:");
 	  for(Volume v : threes){
-	  System.out.println(v);
-	  System.out.println(v.getVolume());
+	      System.out.println(v);
+	      System.out.println(v.getVolume());
 	  }
-	*/
     }
 }
 
