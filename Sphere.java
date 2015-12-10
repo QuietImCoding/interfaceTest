623f36906f2612818ea7025a5897c627a5cf6eff
Daniel Monteagudo <dmonte3210@gmail.com>
2015-12-10 16:42:44 -0500

    Added interface to Sphere.java

--- Sphere.java
+++ Sphere.java
@@ -1,10 +1,14 @@
-public class Sphere extends Circle{
+public class Sphere extends Circle implements Volume{
 
     public Sphere(double radius, String name){
 	super(radius,name);
     }
 
+    public double getVolume() {
+	return 4 * Math.PI * Math.pow(getRadius(), 2) / 3.0;
+    }
+    
     public String toString(){
-	return "FIXME!";
+	return "Radius: " + getRadius();
     }
 }
